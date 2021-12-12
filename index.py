d431f46d3c2af0e900e572766a8874033fc6c15b
Greg Finley <gregory.finley@gmail.com>
2021-12-11 21:06:44 -0800

    Break into smaller functions

--- index.py
+++ index.py
@@ -12,42 +12,64 @@ load_dotenv()
 
 
 def main():
-    DATABASE_URL = os.environ["DATABASE_URL"]
+    oldest_people_table = scrape_wikipedia_oldest_living_people_table()
+    oldest_person_dict = table_to_oldest_person_dict(oldest_people_table)
 
-    conn = psycopg2.connect(DATABASE_URL, sslmode="require")
-    conn.autocommit = True
+    conn = get_database_connection()
+    known_birthdates = find_birthdates_from_database(conn)
 
-    cur = conn.cursor()
+    if oldest_person_dict["Birth date"] not in known_birthdates:
+        tweet_message = generate_tweet_message(oldest_person_dict)
+        send_tweet(tweet_message)
+        add_new_birthdate_to_database(conn, oldest_person_dict)
+    else:
+        print(f"{oldest_person_dict['Name']} is still the oldest person")
 
-    cur.execute(
-        "CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, num integer, data varchar);"
-    )
-
-    cur.close()
     conn.close()
 
-    # Scrape the table
+
+def scrape_wikipedia_oldest_living_people_table():
     wikiurl = "https://en.wikipedia.org/wiki/List_of_the_oldest_living_people"
     response = requests.get(wikiurl)
     print(response.status_code)
 
     soup = BeautifulSoup(response.text, "html.parser")
-    oldest_people_table = soup.find("table", {"class": "wikitable"})
+    return soup.find("table", {"class": "wikitable"})
 
-    # Put it in a pandas dataframe and then the oldest person's info into a dict
+
+def table_to_oldest_person_dict(oldest_people_table):
     oldest_people_df = pd.read_html(str(oldest_people_table))
-    oldest_person_dict = pd.DataFrame(oldest_people_df[0]).iloc[0].to_dict()
+    return pd.DataFrame(oldest_people_df[0]).iloc[0].to_dict()
 
-    # Remove the annotations: Kane Tanaka[3] -> Kane Tanaka
-    oldest_person_dict["Name"] = re.sub(
-        "[\(\[].*?[\)\]]", "", oldest_person_dict["Name"]
-    )
+
+def get_database_connection():
+    DATABASE_URL = os.environ["DATABASE_URL"]
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+    conn.autocommit = True
+
+    return conn
+
+
+def find_birthdates_from_database(conn):
+    with conn.cursor() as curs:
+        curs.execute(
+            "CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, num integer, data varchar);"
+        )
+
+
+def generate_tweet_message(oldest_person_dict):
+    clean_name = re.sub("[\(\[].*?[\)\]]", "", oldest_person_dict["Name"])
+    return f"{clean_name} was born on {oldest_person_dict['Birth date']}"
+
+
+def add_new_birthdate_to_database(conn, oldest_person_dict):
+    with conn.cursor() as curs:
+        curs.execute(
+            "CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, num integer, data varchar);"
+        )
 
     print(oldest_person_dict)
-
-    # Check if the top spot has a birthday we haven't seen in our database
-
-    # If so, tweet about it!
 
 
 def send_tweet(message):
