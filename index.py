b65750cece26f054344310ece0fe977f8314683e
Greg Finley <gregory.finley@gmail.com>
2021-12-11 19:37:53 -0800

    Add Mailgun error alert

--- index.py
+++ index.py
@@ -9,35 +9,61 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.environ["DATABASE_URL"]
 
-conn = psycopg2.connect(DATABASE_URL, sslmode="require")
-conn.autocommit = True
+def main():
+    DATABASE_URL = os.environ["DATABASE_URL"]
 
-cur = conn.cursor()
+    conn = psycopg2.connect(DATABASE_URL, sslmode="require")
+    conn.autocommit = True
 
-cur.execute(
-    "CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, num integer, data varchar);"
-)
+    cur = conn.cursor()
 
-cur.close()
-conn.close()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS test (id serial PRIMARY KEY, num integer, data varchar);"
+    )
 
-# Scrape the table
-wikiurl = "https://en.wikipedia.org/wiki/List_of_the_oldest_living_people"
-response = requests.get(wikiurl)
-print(response.status_code)
+    cur.close()
+    conn.close()
 
-soup = BeautifulSoup(response.text, "html.parser")
-table = soup.find("table", {"class": "wikitable"})
+    # Scrape the table
+    wikiurl = "https://en.wikipedia.org/wiki/List_of_the_oldest_living_people"
+    response = requests.get(wikiurl)
+    print(response.status_code)
 
-# Put it in a pandas dataframe
-df = pd.read_html(str(table))
-df = pd.DataFrame(df[0])
-# Remove the annotations: Kane Tanaka[3] -> Kane Tanaka
-df["Name"] = df["Name"].apply(lambda x: re.sub("[\(\[].*?[\)\]]", "", x))
-print(df.head())
+    soup = BeautifulSoup(response.text, "html.parser")
+    table = soup.find("table", {"class": "wikitable"})
 
-# Check if the top spot has a birthday we haven't seen in our database
+    # Put it in a pandas dataframe
+    df = pd.read_html(str(table))
+    df = pd.DataFrame(df[0])
+    # Remove the annotations: Kane Tanaka[3] -> Kane Tanaka
+    df["Name"] = df["Name"].apply(lambda x: re.sub("[\(\[].*?[\)\]]", "", x))
+    print(df.head())
 
-# If so, tweet about it!
+    # Check if the top spot has a birthday we haven't seen in our database
+
+    # If so, tweet about it!
+
+
+def send_email(subject, message):
+    print("Sending email about exception")
+    print(message)
+    mailgun_domain = os.environ["MAILGUN_DOMAIN"]
+    return requests.post(
+        f"https://api.mailgun.net/v3/{mailgun_domain}/messages",
+        auth=("api", os.environ["MAILGUN_API_KEY"]),
+        data={
+            "from": f"Heroku Error <heroku.error@{mailgun_domain}>",
+            "to": [os.environ["EMAIL_TO"]],
+            "subject": subject,
+            "text": message,
+        },
+    )
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        if os.environ.get("EMAIL_TO"):
+            send_email("Oldest Living Person execution error", e)
