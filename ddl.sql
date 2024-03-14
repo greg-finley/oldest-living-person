20babfc002fd158fd2d98c9fb9da23f802034678
Greg Finley <gregory.finley@gmail.com>
2024-03-14 15:51:11 -0700

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "known_birthdates" (
   "id" int NOT NULL AUTO_INCREMENT,
-  "birth_date_epoch" int DEFAULT NULL,
-  "times_seen" int DEFAULT NULL,
-  "tweeted" tinyint(1) DEFAULT NULL,
+  "birth_date_epoch" int NOT NULL,
+  "times_seen" int NOT NULL,
+  "tweeted" tinyint(1) NOT NULL,
   PRIMARY KEY ("id")
-);
+)
